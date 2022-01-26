011b9084a2477f31c475eef685df8c851271c678
Gallelee <thimotus@gmail.com>
2022-01-26 18:59:17 -0500

    Add blowout effect to landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,5 +1,5 @@
-import React,{useState} from "react"
-import { animated,useSpring } from "react-spring"
+import React, { useState } from "react"
+import { animated, useSpring } from "react-spring"
 import Navbar from "./Navbar"
 import NewBlackCircle from "../images/NewBlackCircle.png"
 import "../styles/homePage.css"
@@ -9,27 +9,28 @@ import { Link } from "react-router-dom"
 import Bubbles from "./Bubbles"
 
 
-export default function LandingPage(){
+export default function LandingPage() {
     const [open, toggle] = useState(false)
-    const effs = useSpring({from:{transform:"translateY(200%)"}, to:{transform: open? "translateY(0%)":"translateY(200%)",opacity: open? 1 : 0}})
-    const leftEffs = useSpring()
-
-    return(
-
+    const effs = useSpring({ from: { transform: "translateY(200%)" }, to: { transform: open ? "translateY(0%)" : "translateY(200%)", opacity: open ? 1 : 0 } })
+    const leftEffs = useSpring({ from: { transform: "translate(100%,-90%)" }, to: { transform: open ? "translate(0%,0%)" : "translate(100%,-90%)", opacity: open ? 1 : 0 } })
+    const rightEffs = useSpring({ from: { transform: "translate(-100%,-90%)" }, to: { transform: open ? "translate(0%,0%)" : "translate(-100%,-90%)", opacity: open ? 1 : 0 } })
+    return (
         <div id="home">
-        <Navbar />
-        <animated.div style={effs} className="top-bubble">
-            <Link to="/userGraph"><Bubbles image={blueCircle} label={"Connects"} startOp={.2} /></Link>
-        </animated.div>
-        <animated.div className="center-bubble" onMouseDown={()=> toggle(!open)}>
-            <Bubbles label={"In-Touch"} image={NewBlackCircle} startOp={1} />
-        </animated.div>
-        <div className="bottom-bubbles">
-            <div id="left-btm-bubble">
-                <Bubbles image={NewBlackCircle} label={"Profile"} startOp={.2} />
+            <Navbar />
+            <animated.div style={effs} className="top-bubble">
+                <Link to="/userGraph"><Bubbles image={blueCircle} label={"Connects"} startOp={.2} /></Link>
+            </animated.div>
+            <animated.div className="center-bubble" onMouseDown={() => toggle(!open)}>
+                <Bubbles label={"In-Touch"} image={NewBlackCircle} startOp={1} />
+            </animated.div>
+            <div className="bottom-bubbles">
+                <animated.div style={leftEffs} id="left-btm-bubble">
+                    <Bubbles image={NewBlackCircle} label={"Profile"} startOp={.2} />
+                </animated.div>
+                <animated.div style={rightEffs}>
+                    <Bubbles label={"About-Us"} image={NewBlackCircle} startOp={.2} />
+                </animated.div>
             </div>
-            <Bubbles label={"About-Us"} image={NewBlackCircle} startOp={.2} />
         </div>
-    </div>
     )
 }
