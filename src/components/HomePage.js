fd85391c483847fd1a0070aa753bfcdc04c01a57
Gallelee <thimotus@gmail.com>
2022-01-26 11:46:06 -0500

    REdo styling and add homepage content

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,14 +1,30 @@
 import React from "react"
 import Navbar from "./Navbar"
-import {animated, useSpring} from "react-spring"
+import "../styles/homePage.css"
+import { animated, useSpring } from "react-spring"
+import { useState } from "react/cjs/react.development"
 
-export default function HomePage(){
-    
-    
+export default function HomePage() {
+    const [text1, toggle1] = useState(false)
+    const [text2, toggle2] = useState(false)
 
-    return(
+    const effs = useSpring({opacity: text1?1:0.2})
+    const effs2 = useSpring({opacity: text2?1:0.2})
+    return (
         <div className="home-page">
-            <Navbar/>
+            <Navbar />
+            <animated.div className="home-text1" style={effs} onMouseOver={() => toggle1(!text1)} onMouseOut={() => toggle1(!text1)}>
+                <h2>Coming Together</h2>
+                <p>These have been trying times. With our lives being busier than ever,
+                    forgetting those we love and cherish has become a regrettable norm.
+                    If we could visualize the links between us and how far they've grown, coming
+                    together could seem the new norm.</p>
+            </animated.div>
+            <animated.div className="home-text2" style={effs2} onMouseOver={()=> toggle2(!text2)} onMouseOut={() => toggle2(!text2)}>
+                <h2>What is In-Touch?</h2>
+                <p>Developed by students attending Brooklyn College, In-Touch is an excercise in interconnectivity.
+                    Designed from the ground up with an ever changing model, to visualize our interactions with those close to us.</p>
+            </animated.div>
         </div>
     )
 }
