3961dc85f4de3e09aceab77e3f0b653fbc396136
Gilman Huang <67421795+Gily-H@users.noreply.github.com>
2022-01-31 18:51:04 -0500

    include home and addfriend button on friend slide

--- src/components/FriendSlide.js
+++ src/components/FriendSlide.js
@@ -1,5 +1,7 @@
 import React from "react";
 import RemoveFriendNode from "./RemoveFriendNode";
+import { Link } from "react-router-dom";
+import "../styles/FriendSlide.css";
 
 export default function FriendSlide(props) {
   const friendId = props.friend.id;
@@ -18,17 +20,34 @@ export default function FriendSlide(props) {
 
   return (
     <div className="friend-slide">
-      <img className="friend-img" src={imageUrl} alt="friend-profile"></img>
-      <h2 className="friend-name">{name}</h2>
+      <div>
+        <img className="friend-img" src={imageUrl} alt="friend-profile"></img>
+        <h2 className="friend-name">{name}</h2>
+        <hr className="friend-hr-line" />
+      </div>
+
       <p className="friend-phone">Phone: {phone}</p>
       <p className="friend-last-contact">Last Connection {updatedAt}</p>
-      <button className="friend-contact" onClick={() => updateStrength(friendId, 20)}>
-        Send a Messge
-      </button>
-      <button className="friend-contact" onClick={() => updateStrength(friendId, -20)}>
-        Wait to connect
-      </button>
+
+      <div className="friend-contact-buttons">
+        <button className="friend-contact message-btn" onClick={() => updateStrength(friendId, 20)}>
+          Send a Messge
+        </button>
+        <button className="friend-contact" onClick={() => updateStrength(friendId, -20)}>
+          Wait to connect
+        </button>
+      </div>
+
       <div className="delete-btn">{displayRemoveButton}</div>
+
+      <div className="friend-slide-footer">
+        <Link className="friend-slide-home-btn" to={`../home`}>
+          <i class="fas fa-home"></i>Home
+        </Link>
+        <Link className="friend-slide-add-friend-btn" to={`../profile/${props.rootUserId}`}>
+          Add Friend
+        </Link>
+      </div>
     </div>
   );
 }
