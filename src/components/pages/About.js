980ee6aedd61f00950fab4567b452e373970b8fb
Gilman Huang <67421795+Gily-H@users.noreply.github.com>
2022-02-03 00:25:28 -0500

    organize files and components

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,29 +1,20 @@
 import React from "react";
-import Navbar from "../Navbar";
-import "../styles/homePage.css";
-import { animated, useSpring } from "react-spring";
-import { useState } from "react/cjs/react.development";
 import "../../styles/about.css";
 
 export default function About() {
-  const [text1, toggle1] = useState(false);
-  const [text2, toggle2] = useState(false);
-
-  const effs = useSpring({ opacity: text1 ? 1 : 0.2, fontSize: text1 ? 40 : 30 });
-  const effs2 = useSpring({ opacity: text2 ? 1 : 0.2 });
   return (
     <div className="home-page">
-      <Navbar />
       <div className="about-circles">
         <div>
-          <img className="profile-pic, about-circle-left" src="https://stringfixer.com/files/18113637.jpg" />
+          <img className="about-circle-left" src="https://stringfixer.com/files/18113637.jpg" alt="bubble" />
           <p className="about-circle-left-text about-circle-allText">
             When you join in-touch, we’ll remind you when it’s been a while since you last connected with your important
             relationships. You can then send them a text, right from here!
           </p>
         </div>
+
         <div>
-          <img className="profile-pic, about-circle-middle" src="https://stringfixer.com/files/18113637.jpg" />
+          <img className="about-circle-middle" src="https://stringfixer.com/files/18113637.jpg" alt="bubble" />
           <p className="about-circle-middle-text about-circle-allText">
             <span className="gray-text">in-Touch</span> is on a mission to help people connect{" "}
             <span className="purple-text">— and stay connected. </span>
@@ -36,8 +27,9 @@ export default function About() {
             </span>
           </p>
         </div>
+
         <div>
-          <img className="profile-pic, about-circle-right" src="https://stringfixer.com/files/18113637.jpg" />
+          <img className="about-circle-right" src="https://stringfixer.com/files/18113637.jpg" alt="bubble" />
           <p className="about-circle-right-text about-circle-allText">
             <span id="creators-text">Creators</span>
             <br></br>
